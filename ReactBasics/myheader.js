849f3ca9a4839b2e506545c2ca9e73630f71630d
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2022-08-09 08:51:40 +0530

    "react basics"

---
+++ ReactBasics/myheader.js
@@ -0,0 +1,7 @@
+const element = React.createElement("h1",draggable=true,"React JS");
+
+const mycontainer = document.querySelector("#root");
+
+//render
+ReactDOM.render(element,mycontainer);
+
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 //import {double as utilsDouble,name} from './utlis.js';
 //import * as utils from './utlis.js';
-import {double,water} from './utils.js'; 
+import double, {default as doubleUtil} from './utils.js'; 
 
-const value = double(5);
-const waterHelper = water();
-console.log(waterHelper);
+console.log(doubleUtil(6));
+
